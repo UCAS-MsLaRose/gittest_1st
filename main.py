311e8ea03919feceafa5921874b460c1ac5eba44
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-01-20 09:32:46 -0700

    made user interface on main

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # Main run file. Holds the User interface
+from creater import char_creater
+
+def main():
+    print("Hello User do you want to create a character?")
+    choice = input("enter 1 to create a character 2 to exit")
+    if choice == "1":
+        char_creater()
+
